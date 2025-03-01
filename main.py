522997de4c5bab6b3ef8079c4052bedc9b8a7f95
nikitos525e34 <n.fokin.bmw@gmail.com>
2025-03-01 15:35:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+from pygame import *
+
+img_back = "galaxy.jpg"
+img_hero = "rocket.png"
+
+win_width = 700
+win_height = 500
+display.set_caption("shoter")
+window = display.set_mode((win_width, win_height))
+backgrount = transform.scale(image.load(img_back),(win_width, win_height))
+
+finish = False
+run = True
+
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play(-1)
+fire_sound = mixer.Sound('fire.ogg')
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x,self.rect.y))
+
+class Player(GameSprite):
+
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    def fire(self):
+        pass
+
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 1)
+
+while run:
+    for e in  event.get():
+        if e .type == QUIT:
+            run = False
+
+    if not finish:
+        window.blit(backgrount, (0, 0))
+        ship.update()
+        ship.reset()
+
+        display.update()
+
+while run:
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    display.update()
+
+    time.delay(50)
