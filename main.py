44b4deaf8002df098c8475695fa735f801acdbf2
nikitos525e34 <n.fokin.bmw@gmail.com>
2025-03-15 15:27:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from pygame import *
-from random import randint #⬅️
+from random import randint
 
-# нам потрібні такі картинки:
-img_back = "galaxy.jpg"  # фон гри
-img_hero = "rocket.png"  # герой
-img_enemy = "ufo.png"  # ворог ⬅️
+img_back = "galaxy.jpg"
+img_hero = "rocket.png"
+img_enemy = "ufo.png"
+img_bullet = "bullet.png"
 
-# створюємо віконце
 win_width = 700
 win_height = 500
 display.set_caption("Shooter")
@@ -23,10 +22,12 @@ mixer.music.play(-1)
 fire_sound = mixer.Sound('fire.ogg')
 
 font.init()
-font2 = font.Font(none, 36)
+font2 = font.Font(None, 36)
 
-score = 0  # збито кораблів⬅️
-lost = 0  # пропущено кораблів⬅️
+score = 0
+lost = 0
+goal = 15
+max_lost = 5
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y,  # один рядок
@@ -42,11 +43,8 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-
-# клас головного гравця
 class Player(GameSprite):
 
-    # метод для керування спрайтом стрілками клавіатури
     def update(self):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
@@ -54,26 +52,27 @@ class Player(GameSprite):
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
 
-    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
-
-# клас спрайта-ворога⬅️
 class Enemy(GameSprite):
     # рух ворога
     def update(self):
         self.rect.y += self.speed
         global lost
-        # зникає, якщо дійде до краю екрана
+
         if self.rect.y > win_height:
             self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
             lost = lost + 1
 
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
 
-
-# створюємо спрайти
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
 monsters = sprite.Group()
@@ -81,11 +80,18 @@ for i in  range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
     monsters.add(monster)
 
+bullets = sprite.Group()
+
 while run:
-    # подія натискання на кнопку Закрити
+
     for e in event.get():
         if e.type == QUIT:
             run = False
+
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
 
     if not finish:
         window.blit(background, (0, 0))
@@ -102,6 +108,9 @@ while run:
         monsters.update()
         monsters.draw(window)
 
+        bullets.update()
+        bullets.draw(window)
+
         display.update()
-    # цикл спрацьовує кожні 0.05 секунд
+
     time.delay(50)
